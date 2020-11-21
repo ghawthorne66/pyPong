1dbc74100fd7e3cb7dfaa4082e8a747f26ecaec9
Greg Hawthorne <ghawthorne@me.com>
2020-11-21 20:17:06 +0100

    add incremental speed to game

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
 screen.bgcolor("green")
 screen.setup(800, 600)
-screen.tracer(0)
 screen.title("Pong")
+screen.tracer(0)
 
-r_paddle = Paddle((350, 0))
-l_paddle = Paddle((-350, 0))
-
-ball = Ball((0, 0))
+r_paddle = Paddle((360, 0))
+l_paddle = Paddle((-360, 0))
+ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -22,8 +23,28 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(.05)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
+
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with right paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Detect when right paddle misses
+    if ball.xcor() > 400:
+        ball.reset_position()
+        time.sleep(1)
+        scoreboard.l_point()
+
+    # Detect when left paddle misses
+    if ball.xcor() < -410:
+        ball.reset_position()
+        time.sleep(1)
+        scoreboard.r_point()
 
 screen.exitonclick()
---
+++ scoreboard.py
@@ -0,0 +1,36 @@
+from turtle import Turtle
+
+ALIGN = "center"
+FONT = ("Arial", 36, "normal")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.l_score = 0
+        self.r_score = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(-100, 260)
+        self.write(f"{self.l_score}", align=ALIGN, font=FONT)
+        self.goto(100, 260)
+        self.write(f"{self.r_score}", align=ALIGN, font=FONT)
+
+    def l_point(self):
+        self.l_score += 1
+        self.update_scoreboard()
+
+    def r_point(self):
+        self.r_score += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+
